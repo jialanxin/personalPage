2f99db2827789f8d6f6bdaa264d6746924e2c087
jialanxin <jialanxin1996@hotmail.com>
2020-03-20 13:13:55 +0800

    change the style of moments

--- pages/moments.vue
+++ pages/moments.vue
@@ -2,13 +2,16 @@
   <v-container>
     <v-row>
       <v-col>
-        <v-timeline>
-          <v-timeline-item>
-            <template v-slot:opposite>
-              <span>2020-03-19</span>
-            </template>
+        <v-timeline dense >
+          <v-timeline-item fill-dot class="white--text">
+            <template v-slot:icon>
+          <span>3月</span>
+        </template>
+             
+           
             <v-card>
                 <v-img src="/img/Moments/2020-03-19/1.webp"></v-img>
+                <v-card-title>2020-03-19</v-card-title>
                 <v-card-text>打过了零灯杀生院，手机电池杀手</v-card-text>
             </v-card>
           </v-timeline-item>
