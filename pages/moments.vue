f55b1699058b12887768fca7589670711c2ba6f1
jialanxin <jialanxin1996@hotmail.com>
2020-03-26 22:51:37 +0800

    write moment

--- pages/moments.vue
+++ pages/moments.vue
@@ -2,17 +2,16 @@
   <v-container>
     <v-row>
       <v-col>
-        <v-timeline dense >
-          <v-timeline-item fill-dot class="white--text">
+        <v-timeline dense>
+          <v-timeline-item v-for="moment in moments" :key=moment.key fill-dot class="white--text">
             <template v-slot:icon>
-          <span>3月</span>
-        </template>
-             
-           
+              <span>{{moment.icon}}</span>
+            </template>
+
             <v-card>
-                <v-img src="/img/Moments/2020-03-19/1.webp"></v-img>
-                <v-card-title>2020-03-19</v-card-title>
-                <v-card-text>打过了零灯杀生院，手机电池杀手</v-card-text>
+              <v-img :src=moment.img></v-img>
+              <v-card-title>{{moment.title}}</v-card-title>
+              <v-card-text>{{moment.text}}</v-card-text>
             </v-card>
           </v-timeline-item>
         </v-timeline>
@@ -23,6 +22,26 @@
 
 <script>
 export default {
+  data() {
+    return {
+      moments: [
+        {
+          img: "/img/Moments/2020-03-26/1.webp",
+          title: "2020-03-26",
+          text: "打帝王花，帝王花耐性低了之后用两个始皇帝宝具加梅林爆伤输出，一张中间红卡150万，尾红不知道多少伤害。",
+          icon: "3月",
+          key:2
+        },
+        {
+          img: "/img/Moments/2020-03-19/1.webp",
+          title: "2020-03-19",
+          text: "打过了零灯杀生院，手机电池杀手",
+          icon: "3月",
+          key:1
+        }
+      ]
+    };
+  },
   head() {
     return {
       title: "个人工具"
