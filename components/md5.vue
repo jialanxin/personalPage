abf2de6bc9a792845527564acaa110cf73e3d8ea
jialanxin <jialanxin1996@hotmail.com>
2020-03-28 23:18:03 +0800

    add a md5 tool

---
+++ components/md5.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card class="elevation-12">
+    <v-toolbar color="primary" dark flat>
+      <v-toolbar-title>MD5</v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-col>
+            <v-text-field label="Value+Key" v-model="value" @keyup.enter="tomd5"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row align="center" justify="center">
+            <p>{{hex}}</p>
+        </v-row>
+      </v-container>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import md5 from "blueimp-md5"
+export default {
+  name: "md5",
+  data() {
+    return {
+      value: "",
+      hex: "",
+    };
+  },
+  methods: {
+      tomd5(){
+          this.hex = md5(this.value)
+      }
+    }
+};
+</script>
--- pages/Tools.vue
+++ pages/Tools.vue
@@ -1,23 +1,28 @@
 <template>
-<v-container>
-  <v-row>
-    <v-col class="text-center">
-      <OpticalUnitConvert />
-    </v-col>    
-  </v-row>
-    
-</v-container>
-
-  
+  <v-container>
+    <v-row>
+      <v-col class="text-center">
+        <md5 />
+        
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+<OpticalUnitConvert />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import OpticalUnitConvert from '../components/OpticalUnitConvert'
+import OpticalUnitConvert from "../components/OpticalUnitConvert";
+import md5 from "../components/md5.vue";
 export default {
-  components:{OpticalUnitConvert},
-  head(){
-    return{
-    title:"个人工具"}
+  components: { OpticalUnitConvert, md5 },
+  head() {
+    return {
+      title: "个人工具"
+    };
   }
-}
+};
 </script>
