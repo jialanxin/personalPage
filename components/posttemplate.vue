c93ebea7c264d3e6d0374d1e47154e5674435a2b
jialanxin <jialanxin1996@hotmail.com>
2020-04-13 00:15:32 +0800

    把post页面提炼成组件

---
+++ components/posttemplate.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>{{posttitle}}</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <p class="font-weight-light">latest update: {{lastUpdate}}</p>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+          <slot name="content"></slot>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import json from "../pages/posts.json";
+export default {
+    props:["posttitle"],
+  data() {
+    return {
+      
+      lastUpdate: ""
+    };
+  },
+  created() {
+    this.lastUpdate = json.find(post => {
+      return post.title === this.posttitle;
+    }).lastUpdate;
+  },
+};
+</script>
---
+++ pages/posts/CreateAurPkg.vue
@@ -0,0 +1,25 @@
+<template>
+<posttemplate :posttitle="title">
+    <template v-slot:content>
+        <p></p>
+    </template>
+</posttemplate>
+</template>
+
+<script>
+import posttemplate from "../../components/posttemplate";
+export default {
+    components:{posttemplate},
+  data() {
+    return {
+      title: "创建一个aur包",    
+    };
+  },
+
+  head() {
+    return {
+      title: this.title
+    };
+  }
+};
+</script>
