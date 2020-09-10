4dfc0d1d513c17716c87e585a250aa1e2f51bec0
jialanxin <jialanxin1996@hotmail.com>
2020-09-10 21:01:43 +0800

    Note: ReadingSept2020

--- pages/notes/ReadingSept2020.vue
+++ pages/notes/ReadingSept2020.vue
@@ -14,13 +14,14 @@
       <v-col>
         <v-expansion-panels multiple v-model="expanded">
           <ReadingPanel>
-            <template v-slot:chinese_title>二维材料中可用于单光子发射的二能级量子系统</template>
+            <template v-slot:chinese_title>二维范德瓦尔斯异质结中偶极子耦合之外的亮激子和暗激子的超快能量转移</template>
             <template
               v-slot:english_title
-            >Two-Level Quantum Systems in Two-Dimensional Materials for Single Photon Emission</template>
-            <template v-slot:author>Gupta 2019</template>
+            >Ultrafast Energy Transfer of Both Bright and Dark Excitons in 2D van der Waals Heterostructures Beyond Dipolar Coupling</template>
+            <template v-slot:author>Wu 2019</template>
             <template v-slot:content>
-           <p></p>
+           <p>MoTe2/WSe2异质结中，能带以type I形式对齐，MoTe2带隙包裹在WSe2的带隙中。</p>
+           <p>比较单体和异质结的静态反射谱，异质结相当于两种材料的峰的加和，仅存在一点红移，原因可能是激子介电环境的改变。</p>
 	   </template>
           </ReadingPanel>
         </v-expansion-panels>
