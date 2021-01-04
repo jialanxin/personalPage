49a090c6570ff7f451575b13e71861d07364a807
jialanxin <jialanxin1996@hotmail.com>
2021-01-04 05:19:36 +0000

    Fund: Buy 164902 519755 519752

--- pages/Fund.vue
+++ pages/Fund.vue
@@ -51,7 +51,7 @@ export default {
           name: "交银信用添利债券(LOF)",
           index: "164902",
           unitValue: 1.1639,
-          alipayQuantity: 315.80,
+          alipayQuantity: 331.20,
           wechatQuantity: 0,
           percent: 13.01,
         },
@@ -92,7 +92,7 @@ export default {
           index: "519755",
           unitValue: 1.397,
           alipayQuantity: 50.01,
-          wechatQuantity: 84.44,
+          wechatQuantity: 86.61,
           percent: 6.58,
         },
         {
@@ -108,7 +108,7 @@ export default {
           index: "519752",
           unitValue: 1.363,
           alipayQuantity: 43.98,
-          wechatQuantity: 62.83,
+          wechatQuantity: 64.91,
           percent: 5.56,
         },
         {
